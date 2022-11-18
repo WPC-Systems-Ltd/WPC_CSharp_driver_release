085829a7d741e1d495abb9d6c37db9eb0cd463eb
chungleepeople <jinhong2009885@gmail.com>
2022-11-18 10:15:26 +0800

    Update

--- WPC_example_code.cs
+++ WPC_example_code.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WPC_Product;
 public class WPC_example_code
 {
     #region ProductMessage
