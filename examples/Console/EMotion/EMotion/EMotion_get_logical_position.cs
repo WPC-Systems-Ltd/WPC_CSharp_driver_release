2c125d692fd5a1720a6de2fee0ee75e95416c0c6
chunglee_people <45706135+chungleepeople@users.noreply.github.com>
2022-12-06 17:52:12 +0800

    v0.1.2 - Add motion partial API  (#22)
    v0.1.2 Date: 2022/12/06, Developer: Chunglee_people 
    ---------------------------------------------------- 
    
    ### Added
    - Add motion simple example code `EMotion_get_logical_position.cs`
    - Add I/P function table in docfx
    
    ### Changed
    - Uniform USB DAQ name `F1`

---
+++ examples/Console/EMotion/EMotion/EMotion_get_logical_position.cs
@@ -0,0 +1,64 @@
+
+/// <summary>
+/// EMotion_get_logical_position.cs
+///
+/// For other examples please check:
+/// https://github.com/WPC-Systems-Ltd/WPC_CSharp_driver_release/tree/main/examples
+/// See README.md file to get detailed usage of this example.
+/// 
+/// Copyright (c) 2022 WPC Systems Ltd.
+/// All rights reserved.
+///  
+/// </summary>
+
+using WPC.Product;
+
+class EMotion_get_logical_position
+{
+    static public void Main()
+    { 
+        Console.WriteLine("Start example code...");
+
+        // Get C# driver version
+        Console.WriteLine($"{Constant.PKG_FULL_NAME} - Version {Constant.VERSION}");
+
+        // Create device handle
+        EMotion dev = new EMotion();
+
+        // Connect to device
+        dev.connect("192.168.1.110");
+ 
+        // Perform DAQ basic information 
+        try
+        {  
+            int status;
+            string[] driver_info = dev.Sys_getDriverInfo();
+            Console.WriteLine($"Model name: {driver_info[0]}");
+            Console.WriteLine($"Firmware version: {driver_info.Last()}");
+
+            for (int i = 0; i < 1000; i++) 
+            {   
+                status = dev.Motion_setLogicalPostion(Constant.EMOTION_CONNECTOR_0, Constant.EMOTION_AXIS_X, i);
+                if (status != 0)
+                {
+                    Console.WriteLine($"Motion_setLogicalPostion status: {status}"); 
+                }
+                
+                int posi = dev.Motion_GetLogicalPostion(Constant.EMOTION_CONNECTOR_0, Constant.EMOTION_AXIS_X);
+                Console.WriteLine($"Motion_GetLogicalP3ostion: {posi}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+        // Disconnect device
+        dev.disconnect();
+
+        // Release device handle
+        dev.close();
+
+        Console.WriteLine("End example code...");
+    }
+}
