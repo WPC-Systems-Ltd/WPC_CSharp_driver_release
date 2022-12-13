37f0e1a71d0fdaf1467e14513feaa1d3d522fbc4
chunglee_people <45706135+chungleepeople@users.noreply.github.com>
2022-12-13 11:44:58 +0800

    v0.1.3 - Complete one-axis relative APIs (#25)
    v0.1.3 Date: 2022/12/13, Developer: Chunglee_people 
    ---------------------------------------------------- 
    
    ### Added
    - WPC_MCX_H_Motion_Manual_r24.pdf
    - Add motion one axis move example code `EMotion_get_logical_position.cs`
    - Add Emotion product into I/O Function Table

---
+++ examples/Console/EMotion/Motion/Motion_get_logical_position.cs
@@ -0,0 +1,64 @@
+/// <summary>
+/// Motion_get_logical_position.cs
+///
+/// For other examples please check:
+/// https://github.com/WPC-Systems-Ltd/WPC_CSharp_driver_release/tree/main/examples
+/// See README.md file to get detailed usage of this example.
+/// 
+/// Copyright (c) 2022 WPC Systems Ltd.
+/// All rights reserved.
+///  
+/// </summary>
+
+using WPC.Product;
+
+class EMotion_get_logical_position
+{
+    static public void Main()
+    { 
+        Console.WriteLine("Start example code...");
+
+        // Get C# driver version
+        Console.WriteLine($"{Constant.PKG_FULL_NAME} - Version {Constant.VERSION}");
+
+        // Create device handle
+        EMotion dev = new EMotion();
+
+        // Connect to device
+        dev.connect("192.168.1.110");
+ 
+        // Perform DAQ basic information 
+        try
+        {  
+            int status;
+            int port = 0;
+            string[] driver_info = dev.Sys_getDriverInfo();
+            Console.WriteLine($"Model name: {driver_info[0]}");
+            Console.WriteLine($"Firmware version: {driver_info.Last()}");
+
+            for (int i = 0; i < 1000; i++) 
+            {   
+                status = dev.Motion_setLogicalPostion(port, Constant.MOTION_AXIS_1, i);
+                if (status != 0)
+                {
+                    Console.WriteLine($"Motion_setLogicalPostion status: {status}"); 
+                }
+                
+                int posi = dev.Motion_GetLogicalPostion(port, Constant.MOTION_AXIS_1);
+                Console.WriteLine($"Motion_GetLogicalP3ostion: {posi}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+        // Disconnect device
+        dev.disconnect();
+
+        // Release device handle
+        dev.close();
+
+        Console.WriteLine("End example code...");
+    }
+}
